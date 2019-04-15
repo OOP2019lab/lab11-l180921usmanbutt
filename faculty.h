f1e7f77a767f4a015b65b24061dbd0a97cea8923
l180921usmanbutt <47095812+l180921usmanbutt@users.noreply.github.com>
2019-04-15 10:12:20 +0500

    Add files via upload

---
+++ faculty.h
@@ -0,0 +1,20 @@
+#include "person.h"
+
+
+
+class faculty:public person 
+{
+private:
+	int course_count;
+	string tele_ext;
+public:
+	faculty(string,string,int,int,string);
+	~faculty();
+	void setcc(int c);
+	int getcc();
+	void setage(int);
+	int getage();
+	string gettele();
+	void settele(string);
+	void printfaculty();
+};
